219aa5fb6bb1efa2cc8b3adfb8dbfeae662f40ce
Dani <neollob@yahoo.es>
2019-07-11 00:25:30 +0200

    3rd commit - refactored

--- http3 (Material)/src/app/users/users-list/users-list.component.ts
+++ http3 (Material)/src/app/users/users-list/users-list.component.ts
@@ -22,16 +22,7 @@ export class UsersListComponent implements OnInit {
   getUsers() {
     this.api.getUsers$().subscribe(arg => {
       this.users = arg;
-      this.dataSource = new MatTableDataSource(this.users);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-      this.dataSource.sortingDataAccessor = (item: User, property) => {
-        switch (property) {
-          case 'name.first': return item.name.first;
-          case 'name.last': return item.name.last;
-          default: return item[property];
-        }
-      };
+      this.matTable();
     });
   }
   deleteUser(id: string) {
@@ -39,6 +30,23 @@ export class UsersListComponent implements OnInit {
       this.data = res;
       this.getUsers();
     });
+  }
+  matTable() {
+    this.dataSource = new MatTableDataSource(this.users);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.dataSource.sortingDataAccessor = (item: User, property) => {
+      switch (property) {
+        case 'name.first': return item.name.first;
+        case 'name.last': return item.name.last;
+        default: return item[property];
+      }
+    };
+  }
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
   }
   ngOnInit() {
     this.getUsers();
